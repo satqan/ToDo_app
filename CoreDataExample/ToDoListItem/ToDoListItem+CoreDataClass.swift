cd0567ec2f49550c54bc4bcb6d40fdd42775f0fd
Чингиз Саткан <cingizsatkan@MacBook-Air-Cingiz.local>
2022-04-04 17:55:51 +0600

    first commit

---
+++ CoreDataExample/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  CoreDataExample
+//
+//  Created by Чингиз Саткан on 04.04.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
